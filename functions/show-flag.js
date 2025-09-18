8264336f9c400e453c0842b4e8eac9f1234ab9f2
sir871364 <126338083+sir871364@users.noreply.github.com>
2025-09-18 19:22:08 +0800

    Update show-flag.js

--- functions/show-flag.js
+++ functions/show-flag.js
@@ -1,4 +1,7 @@
 export const onRequestGet = ({ env }) =>
   new Response(JSON.stringify({ enabled: env.SHOW_LOGIN_INFO === "true" }), {
-    headers: { "content-type": "application/json; charset=utf-8", "cache-control": "no-store" },
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store"
+    },
   });
